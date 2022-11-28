55d66d115ba0f20ad7960e76077548971e5c6137
Vincent Terrasi <vt3ktek@hotmail.com>
2022-11-28 07:48:31 -0800

    Update 02-05-rstudio-cloud.R

--- Chapitre 2/02-05-rstudio-cloud.R
+++ Chapitre 2/02-05-rstudio-cloud.R
@@ -8,24 +8,5 @@
 
 # Créez un nouveau script R avec le nom `sherlock-holmes.R`.
 
-### Exercice 3
 
-# Créez un nouveau document R Markdown avec le nom `sherlock-holmes.Rmd`.
-
-### Exercice 4
-
-# Créez un nouveau document R Markdown avec le nom `sherlock-holmes.Rmd` et le format de sortie `pdf_document`.
-
-# Ajoutez le code suivant dans l'en-tête YAML :
-  
-```
-title: "Sherlock Holmes"
-author: "Arthur Conan Doyle"
-output:
-  pdf_document:
-  fig_caption: yes
-number_sections: yes
-toc: yes
-toc_depth: 2
-```
 
