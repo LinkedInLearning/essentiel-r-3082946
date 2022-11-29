86e072a3ca254b59e427e8b8783167f734d7e5f8
Vincent Terrasi <vt3ktek@hotmail.com>
2022-11-29 02:12:52 -0800

    Update 03-04-chaine-test.R

--- Chapitre 3/03-04-chaine-test.R
+++ Chapitre 3/03-04-chaine-test.R
@@ -2,19 +2,29 @@
 # Dans le package base
 
 # 1 is.character() and as.character()
+perso <- 1
+is.character(perso)
+
+perso2 <- as.character(perso)
+is.character(perso2)
 
 # 2 tolower() and toupper()
+perso3 <- "sherlock holmes"
+toupper(perso3)
 
 # 3 nchar()
+nchar(perso3)
 
-# 4 str_count()
+# 4 grep()
+grep('v', perso3)
 
-# 5 grep()
+# 5 paste() et paste0()
+paste0('sherlock','holmes')
+paste('sherlock','holmes')
 
-# 6 paste()
+# 6 sub() and gsub()
+sub('e','u',perso3)
+gsub('e','u',perso3)
 
-# 7 strsplit()
-
-# 8 sub() and gsub()
-
-# 9 substr()
+# 7 substr()
+substr(perso3,1,5)
