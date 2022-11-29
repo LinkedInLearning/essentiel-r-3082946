f787e233bd07060f8823f66085b9ce0077736e1a
Vincent Terrasi <vt3ktek@hotmail.com>
2022-11-29 07:16:14 -0800

    Update 04-03-dplyr-pro.R

--- Chapitre 4/04-03-dplyr-pro.R
+++ Chapitre 4/04-03-dplyr-pro.R
@@ -8,11 +8,11 @@ select(iris, Sepal.Length, Sepal.Width)
 
 # rename() : renommer des variables ;
 
-rename(iris, SL = Sepal.Length, SW = Sepal.Width)
+a <- rename(iris, SL = Sepal.Length, SW = Sepal.Width)
 
 # filter() : sélectionner des observations selon une ou plusieurs conditions ;
 
-filter(iris, Sepal.Length > 5)
+b <- filter(iris, Sepal.Length > 5)
 
 # arrange() : trier la table selon une ou plusieurs variables ;
 
@@ -20,12 +20,14 @@ arrange(iris, Sepal.Length)
 
 # mutate() : ajouter des variables qui sont fonction d’autres variables ;
 
-mutate(iris, ratio = Sepal.Length / Sepal.Width)
+c <- mutate(iris, ratio = Sepal.Length / Sepal.Width)
 
 # summarise() : calculer une statistique à partir de données ;
 
 summarise(iris, mean(Sepal.Length))
 
 # group_by() : faire des opérations par groupe.
+g <- group_by(iris, Species) %>%
+  summarise(mean(Sepal.Length))
 
-group_by(iris, Species)
+
