7bb3ed75ac39d1c7e6c32cda614bf4ee1cd44da1
Vincent Terrasi <vt3ktek@hotmail.com>
2022-11-29 01:15:03 -0800

    Update 03-02-vecteurs.R

--- Chapitre 3/03-02-vecteurs.R
+++ Chapitre 3/03-02-vecteurs.R
@@ -1,6 +1,6 @@
 # Présentation du concept de vecteur et première utilisation pratique avec des exemples concrets
 
-# c pour colmun
+# c pour column
 x <- c(1,3,6,9,10)
 
 x <- 1:4
@@ -9,22 +9,18 @@ x
 # ajout 2 à tout
 x + 2 
 
-# miultiplier tout par 2
+# multiplier tout par 2
 x * 2
 
-# chaque composante au carré
-
-x*y # multiplication de adama ou division de adama
-
+#
 t <- 1:7
 
 z <- x+t
 # erreur si pas la meme taille
 
 # sum(x)
-# cumsum(x)
 
-# diff(x)
+# cumsum(x)
 
 # max(x)
 
