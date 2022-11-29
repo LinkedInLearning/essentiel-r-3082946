7a09c3133aed8f4bbc8078c0bc6ad8e227e29e88
Vincent Terrasi <vt3ktek@hotmail.com>
2022-11-29 04:52:49 -0800

    Update 03-06-liste.R

--- Chapitre 3/03-06-liste.R
+++ Chapitre 3/03-06-liste.R
@@ -1,27 +1,24 @@
 # Apprendre à créer des listes et découverte des fonctions utiles applicables sur des listes
 
-
 # Création de listes
 # La fonction de base pour créer une liste est la fonction  list   :
   
-personList <- list(c("Sherlock","Watson","C","A"),matrix(1:4,2,2))
+personList <- list(c("Sherlock","Watson","Dr Watson","James Moriarty"),matrix(1:6,2,3))
 personList
-
 
 length(personList)
 
-mode(personList)
-
 is.list(personList)
 
-#  y accéder plus facilement via l’opérateur  $  . Ceci est faisable via la fonction  names  :
+#  y accéder plus facilement via l’opérateur  $  . 
+# Ceci est faisable via la fonction  names  :
   
 names(personList) # pas de nom actuellement, la fonction retourne un NULL
 
-
-names(personList) <- c("nom","age")
+names(personList) <- c("nom","matrice")
 names(personList)
 
+personList$
 
 # Il est également possible de créer une liste en partant d’une liste vide.
 
@@ -32,23 +29,22 @@ li
 li[[1]] <- 1:4
 li
 
-li$nouv <- matrix(1:4,nrow=2)
+li$matrice <- matrix(1:4,nrow=2)
 li
 
-# Comme la première composante n’a pas de nom, on retrouve  [[1]]  dans l’affichage de la liste puis la composante  nouv  .
-
 names(li)
-# [1] "" "nouv"
-# et les attributs de cette liste sont les noms.
 
 # Fonctions utiles
 # il n’est pas possible de faire des calculs entre plusieurs listes. 
 # Néanmoins, il existe quelques fonctions valides et utiles :
   
-# lapply  applique une fonction (comme la moyenne, la variance, etc.) successivement à chacune des composantes.
+# lapply  applique une fonction (comme la moyenne, la variance, etc.) 
+# successivement à chacune des composantes.
+lapply(li, sum)
 
 # unlist(maliste)  crée un seul vecteur contenant tous les éléments de la liste. 
 # Les éléments d’un vecteur étant nécessairement du même mode, il faut faire attention à la conversion automatique pratiquée par R.
+unlist(li)
 
 #concatène deux listes.
-c(liste1,liste2)  
+list2 <- c(personList,li)  
