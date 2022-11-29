283df14efe742fcc8f41381acc2c6a55b7825611
Vincent Terrasi <vt3ktek@hotmail.com>
2022-11-29 04:54:12 -0800

    Update 03-08-op-logique.R

--- Chapitre 3/03-08-op-logique.R
+++ Chapitre 3/03-08-op-logique.R
@@ -27,24 +27,25 @@
 
 # Utile dans les filtres
 
-
 ## L'énigme de Sherlock Holmes
 ## Sherlock Holmes enquête sur un meurtre. Il a trouvé une note avec le message suivant :
-## "Le meurtrier est un homme mesurant entre 170 et 180 cm, il a entre 30 et 40 ans, il porte une barbe et un chapeau."
+## "Le meurtrier est un homme mesurant entre 170 et 180 cm, 
+## il a entre 30 et 40 ans, il porte une barbe et un chapeau."
 ## Sherlock Holmes a trouvé trois suspects :
   
+df <- data.frame(c("Albert","Jacques","Jean"),
+                 c(175,178,173),
+                 c(35,32,41),
+                 c(TRUE,FALSE,TRUE),
+                 c(TRUE,TRUE,TRUE))
 
-suspect1 <- c(175, 35, TRUE, TRUE)
-suspect2 <- c(178, 32, FALSE, TRUE)
-suspect3 <- c(173, 41, TRUE, FALSE)
-
-
-# Les quatre éléments de chaque vecteur sont :
-#   
-# - taille en cm
-# - l'âge
-# - barbe (VRAI ou FAUX)
-# - chapeau (VRAI ou FAUX)
-
+colnames(df) <- c("Prénom","Taille","Age","Barbe", "Chapeau")
 
 ## Qui est le coupable ?
+
+df[which(df$Barbe==TRUE & df$Chapeau==TRUE),]
+
+df[which(df$Barbe==TRUE & df$Chapeau==TRUE & df$Age<40),]
+
+
+
