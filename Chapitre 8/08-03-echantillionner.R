a1340ac6835400c4ee16f5d70c3dfce747dd8cfa
Vincent Terrasi <vt3ktek@hotmail.com>
2022-11-30 07:54:24 -0800

    Update 08-03-echantillionner.R

--- Chapitre 8/08-03-echantillionner.R
+++ Chapitre 8/08-03-echantillionner.R
@@ -13,17 +13,14 @@ x <- 1:1197
 # Mettre les valeurs d'une liste dans un ordre aléatoire
 x <- sample(x) 
 
-
-# Créer une liste de 10000 valeurs suivant la loi normale, moyenne 20, écart-type 3.
-x <- rnorm(10000,20,3) 
-
-
 #Créer une liste de 1000 valeurs suivant une loi uniforme, minimum 20, maximum 80
 intelligence <- runif(1197,20,80) 
 force <- runif(1197,20,80)
 popularité <- runif(1197,80,100)
 
-d_frame$Popularité <- round(popularité)
+intelligence <- round(intelligence)
+force <- round(force)
+popularité <- round(popularité)
 
 # Création d'un sample aléatoire
 sample_data <- sample_n(d_frame, size = 100)
